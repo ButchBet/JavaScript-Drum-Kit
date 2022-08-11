7cdd15bee333ec9462c5cbfcc1d6e7df5825c625
ButchBet <butchbet01@gmail.com>
2022-08-11 13:09:23 -0500

    Linked the instruments in JS in order to modify one clase, at this case each time than the use keydow one key Js will process with the animation of the scale of the instrument.

---
+++ README.md
@@ -0,0 +1,34 @@
+# Wes Bos Courses - Brum Kit solution 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+## Overview
+This is a solution to the [Drum Kit practice challenge](https://courses.wesbos.com/account/access/62f5161388db94aff3b2dab9/view/194130650).
+
+### Screenshot
+![Desktop](./images/desktop.png)
+![Mobile](./images/mobile.png)
+
+### Links
+- Solution URL: [GitHub](https://github.com/ButchBet/JavaScript-Drum-Kit)
+- Live Site URL: [Drum Kit](https://butchbet.github.io/JavaScript-Drum-Kit/)
+
+### Built with
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- JS vanilla
+
+## Author
+- Website - [Butchbet](none)
+- Twitter - [@JustButchBet](https://twitter.com/JustButchBet)
--- js/index.js
+++ js/index.js
@@ -1,63 +1,131 @@
 "use strict";
 
-const key = document.getElementById("key");
-const boom = new  Audio("../sounds/boom.wav");
-const clap = new Audio("../sounds/clap.wav");
-const hihat = new Audio("../sounds/hihat.wav");
-const kick = new Audio("../sounds/kick.wav");
-const openhat = new Audio("../sounds/openhat.wav");
-const ride = new Audio("../sounds/ride.wav");
-const snare = new Audio("../sounds/snare.wav");
-const tink = new Audio("../sounds/tink.wav");
-const tom = new Audio("../sounds/tom.wav"); 
+const boom = new  Audio("sounds/boom.wav");
+const Boom = document.getElementById("boom");
+
+const clap = new Audio("sounds/clap.wav");
+const Clap = document.getElementById("clap");
+
+const hihat = new Audio("sounds/hihat.wav");
+const Hihat = document.getElementById("hihat");
+
+const kick = new Audio("sounds/kick.wav");
+const Kick = document.getElementById("kick");
+
+const openhat = new Audio("sounds/openhat.wav");
+const Openhat = document.getElementById("openhat");
+
+const ride = new Audio("sounds/ride.wav");
+const Ride = document.getElementById("ride");
+
+const snare = new Audio("sounds/snare.wav");
+const Snare = document.getElementById("snare");
+
+const tink = new Audio("sounds/tink.wav");
+const Tink = document.getElementById("tink");
+
+const tom = new Audio("sounds/tom.wav"); 
+const Tom = document.getElementById("tom");
 
 document.addEventListener("keydown", (e) => {
-    console.log(e);
-
     switch(e.key) {
         case "1":
             boom.currentTime = 0;
             boom.play();
+
+            Boom.classList.add("active");
+
+            setTimeout(() => {
+                Boom.classList.remove("active");
+            }, 300);
         break;
 
         case "2":
             clap.currentTime = 0;
             clap.play();
+
+            Clap.classList.add("active");
+
+            setTimeout(() => {
+                Clap.classList.remove("active");
+            }, 300);
         break;
 
         case "3":
             hihat.currentTime = 0;
             hihat.play();
+
+            Hihat.classList.add("active");
+
+            setTimeout(() => {
+                Hihat.classList.remove("active");
+            }, 300);
         break;
 
         case "4":
             kick.currentTime = 0;
             kick.play();
+
+            Kick.classList.add("active");
+
+            setTimeout(() => {
+                Kick.classList.remove("active");
+            }, 300);
         break;
 
         case "5":
             openhat.currentTime = 0;
             openhat.play();
+
+            Openhat.classList.add("active");
+
+            setTimeout(() => {
+                Openhat.classList.remove("active");
+            }, 300);
         break;
 
         case "6":
             ride.currentTime = 0;
             ride.play();
+
+            Ride.classList.add("active");
+
+            setTimeout(() => {
+                Ride.classList.remove("active");
+            }, 300);
         break;
 
         case "7":
             snare.currentTime = 0;
             snare.play();
+
+            Snare.classList.add("active");
+
+            setTimeout(() => {
+                Snare.classList.remove("active");
+            }, 300);
         break;
 
         case "8":
             tink.currentTime = 0;
             tink.play();
+
+            Tink.classList.add("active");
+
+            setTimeout(() => {
+                Tink.classList.remove("active");
+            }, 300);
         break;
 
         case "9":
             tom.currentTime = 0;
             tom.play();
+
+            Tom.classList.add("active");
+
+            setTimeout(() => {
+                Tom.classList.remove("active");
+            }, 300);
         break;
     }
 });
